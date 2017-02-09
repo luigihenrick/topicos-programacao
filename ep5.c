5cef187b7e5420d4bc35a97cc3821023c8dc3e7f
luigihenrick <luigihenrick@gmail.com>
2017-02-09 15:44:36 -0200

    ep5 commit

---
+++ ep5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 10
+#define M 30
+
+char ** ordenacaoCrescente (char palavras[N][M]){
+    char * aux;
+    int i;
+    for(i = 0; i < N; i++){
+        aux = palavras[i];
+        
+    }
+}
+char ** ordenacaoDecrescente (char palavras[N][M]);
+
+int main () {
+    char str [10][M] = {"lovelace", "snyder", "jennings", "meltzer", "wescoff",
+    "bilas", "mcnulthy", "lichterman", "hopper", "hamilton"};
+    char str1 [6][M] = {"heinlein", "gibson", "clarke", "asimov", "wallace", "kindred" };
+    return 0;
+}
